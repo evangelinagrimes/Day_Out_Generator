77f7c3fe1bf2e8950684cc656495f5987c30dfab
Eva <evangelinagrimes@gmail.com>
2025-11-21 11:20:55 -0500

    Implementing GUI

--- generate_activities.py
+++ generate_activities.py
@@ -13,38 +13,36 @@ def generate_activities(time, day, zip, mile_limit, region, activityList):
     desertBool = True 
     return None
 
-client_secret_file = 'client_secret.json'
-API_NAME = 'places'
-API_VERSION = 'v1'
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+    client_secret_file = 'client_secret.json'
+    API_NAME = 'places'
+    API_VERSION = 'v1'
+    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
 
-service = create_service(client_secret_file, API_NAME, API_VERSION, SCOPES)
-
-# Raw Inputs
+    service = create_service(client_secret_file, API_NAME, API_VERSION, SCOPES)
 
 
-query = 'ramen'
-request_body = {
-    'textQuery': query,
-    'regionCode': 'US', 
-    'locationRestriction': {
-        'rectangle': {
-            'low': {
-                'latitude': 40.477398, 
-                'longitude': -74.259087
-            },
-            'high': {
-                'latitude': 40.91618, 
-                'longitude': -73.70018
+    query = 'ramen'
+    request_body = {
+        'textQuery': query,
+        'regionCode': 'US', 
+        'locationRestriction': {
+            'rectangle': {
+                'low': {
+                    'latitude': 40.477398, 
+                    'longitude': -74.259087
+                },
+                'high': {
+                    'latitude': 40.91618, 
+                    'longitude': -73.70018
+                }
             }
-        }
-    },
-    'priceLevels': ['PRICE_LEVEL_INEXPENSIVE', 'PRICE_LEVEL_VERY_EXPENSIVE'] 
-}
+        },
+        'priceLevels': ['PRICE_LEVEL_INEXPENSIVE', 'PRICE_LEVEL_VERY_EXPENSIVE'] 
+    }
 
-response = service.places().searchText(
-    body=request_body,
-    fields='*'
-).execute()
+    response = service.places().searchText(
+        body=request_body,
+        fields='*'
+    ).execute()
 
-print(response)
+    print(response)
---
+++ main.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+from tkinter import ttk
+from generate_activities import generate_activities
+
+# Tutorial referenced https://www.youtube.com/watch?v=mop6g-c5HEY 
+"""
+Notes: 
+# Keep in mind what widgets are dependent on what
+# Use .pack() to officially add the widget to the base
+"""
+
+def trigger_generate_activities():
+    zip = zipcode_int.get()
+
+    # generate_activities()
+
+    output_dinner_string.set('Restaurant')
+    output_activity_string.set('Activity')
+    output_desert_string.set('Desert')
+
+# === WINDOW ===
+window = tk.Tk()
+window.title("Date Night Generator")
+window.geometry('500x300')
+
+# === TITLE ===
+title_label = ttk.Label(master = window, text = "Date Night Generator", font = 'Calibri 24')
+title_label.pack()
+
+# === INPUT FIELDS ===
+input_frame = ttk.Frame(master = window)
+zipcode_int = tk.IntVar()
+zipcode = ttk.Entry(master = input_frame, textvariable = zipcode_int)
+button = ttk.Button(master = input_frame, text = 'Generate', command = trigger_generate_activities)
+zipcode.pack(side= 'left', padx = 10)
+button.pack(side= 'left')
+input_frame.pack(pady = 10)
+
+# === OUTPUT ===
+
+# Header settings
+HEADER_FONT = 'Calibri 15'
+HEADER_SIDE = 'left'
+
+# Output settings
+OUTPUT_FONT = 'Calibri 12'
+
+# Variables and Labels
+output_dinner_string = tk.StringVar()
+output_dinner_label = ttk.Label(master = window, 
+                         text = 'Dinner', 
+                         font = OUTPUT_FONT, 
+                         textvariable = output_dinner_string)
+header_dinner_label = ttk.Label(master= window,
+                            text = "Activity 1", 
+                            font = HEADER_FONT)
+output_dinner_label.pack(side= 'bottom', pady = 15, padx = 5)
+header_dinner_label.pack(side= HEADER_SIDE, padx= 15)
+
+output_activity_string = tk.StringVar()
+output_activity_label = ttk.Label(master = window, 
+                         text = 'Activity', 
+                         font = OUTPUT_FONT, 
+                         textvariable = output_activity_string)
+header_activity_label = ttk.Label(master= window,
+                            text = "Activity 2", 
+                            font = HEADER_FONT)
+output_activity_label.pack(side= 'bottom', pady = 15, padx = 5)
+header_activity_label.pack(side= HEADER_SIDE, padx= 15)
+
+output_desert_string = tk.StringVar()
+output_desert_label = ttk.Label(master = window, 
+                         text = 'output', 
+                         font = OUTPUT_FONT, 
+                         textvariable = output_desert_string)
+header_desert_label = ttk.Label(master= window,
+                            text = "Activity 3", 
+                            font = HEADER_FONT)
+output_desert_label.pack(side= 'bottom', pady = 15, padx = 5)
+header_desert_label.pack(side= HEADER_SIDE, padx= 15)
+
+# === RUN ===
+window.mainloop()
+
